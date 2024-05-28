bb4ac4d21760c396fe76f96c757fb095ed5cf495
carlosotrebla <carlosotrebla@gmail.com>
2024-05-28 18:10:23 -0300

    Configuração de cors para acesso de frontend

--- src/main/java/br/com/serratec/controller/CategoriaController.java
+++ src/main/java/br/com/serratec/controller/CategoriaController.java
@@ -4,13 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.serratec.dtos.CategoriaRequestDTO;
 import br.com.serratec.dtos.CategoriaResponseDTO;
@@ -24,7 +18,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-
 @RestController
 @RequestMapping("/categorias")
 @Tag(name = "CATEGORIAS", description = "API de categorias")
